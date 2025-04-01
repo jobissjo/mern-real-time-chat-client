0b32c7e318ecb437e343d48b527aba205180395b
Jobi S S <jobisjobi1234@gmail.com>
2025-04-01 21:56:56 +0530

    bug fix

--- src/apiCalls/index.js
+++ src/apiCalls/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: import.meta.env.API_URL
+    baseURL: import.meta.env.API_URL ?? 'https://mern-real-time-chat-1.onrender.com'
 })
 
 axiosInstance.interceptors.request.use(
